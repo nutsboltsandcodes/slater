c4d048ced4aac1aeef288316330ca86b05337e00
Wyatt <nutsboltandcodes@gmail.com>
2017-10-12 22:42:18 -0600

    Added a global style

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
         <input type="search" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>"
+            placeholder="<?php echo esc_attr_x( 'Search...', 'slater' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
             title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
     	<input type="submit" class="search-submit"
