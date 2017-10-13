65efa2fd522b7339aed49c0eeb18d0f324cc24b4
Wyatt <nutsboltandcodes@gmail.com>
2017-10-12 23:58:41 -0600

    Added post data to archives/category page

--- archive.php
+++ archive.php
@@ -25,6 +25,27 @@ get_header(); ?>
 				<div class="archive-index">
 			
 						<h1 class="index-headline"><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h1>
+						<?php 
+
+						$comment_count = get_comments_number();
+
+						if ( $comment_count == 0 ) {
+							$comment_count = "";
+						} elseif ( $comment_count == 1) {
+							$comment_count = "| " . $comment_count . " comment";
+						} else {
+							$comment_count = "| " . $comment_count . " comments";
+						} 
+
+						if ( get_the_time() == get_the_modified_time() ) { ?>
+							<small class="muted"><?php the_modified_date('F j, Y'); ?> <?php echo $comment_count; ?> | </small>
+							<small class="muted">Posted In: <?php echo get_the_category_list(', '); ?></small>
+						<?php } else { ?>
+							<small class="muted"><?php the_modified_date('F j, Y'); ?> <?php echo $comment_count; ?> | </small>
+							<small class="muted">Posted In: <?php echo get_the_category_list(', '); ?></small>
+						<?php }
+						?>		
+
 					
 						<p class="index-excerpt"><?php the_excerpt(); ?><!-- Post/Update date -->
         		<?php get_template_part( 'template-parts/post', 'date' );?></p>
