58d2f98c23bd60be1c5ae15829a8f3a77d04872f
Wyatt <nutsboltandcodes@gmail.com>
2017-10-13 18:10:58 -0600

    Added a text domain to a translation function

--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
         <input type="search" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Search...', 'slater' ) ?>"
+            placeholder="<?php echo esc_attr_x( 'Search...', 'none', 'slater' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+            title="<?php echo esc_attr_x( 'Search for:', 'none', 'slater' ) ?>" />
     	<input type="submit" class="search-submit"
-        value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+        value="<?php echo esc_attr_x( 'Search', 'none', 'submit button' ) ?>" />
 </form>
